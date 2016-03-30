09f4e1f3af4ba2789d1b68ed581c6f9a34707bf1
xinwang3 <xin.wang.3@stonybrook.edu>
2016-03-29 23:33:16 -0400

    Update cocao.js

--- cocao.js
+++ cocao.js
@@ -1,7 +1,8 @@
 console.log('cocao.js loaded');
 
 (function(){ // wrapping everything within an annonymous function
-    var h = '<h3 style="color:navy">Fibonacci generator</h3>'
+    var h = '<h3 style="color:brown">Dark Chocolate Survey</h3>'
+    h += '<h4 style="color:brown">What % of cocao do you like the most:</h4>'
     h +='<button id="fibButton" type="button" class="btn btn-primary">Generate</button> '
     
     if(typeof(sbmApps)=='function'){ // if sbmApps library was loaded
