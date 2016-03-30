1a00028df4541c6e4cf21f8e926444526bc80481
xinwang3 <xin.wang.3@stonybrook.edu>
2016-03-29 23:40:57 -0400

    Update cocao.js

--- cocao.js
+++ cocao.js
@@ -3,8 +3,8 @@ console.log('cocao.js loaded');
 (function(){ // wrapping everything within an annonymous function
     var h = '<h3 style="color:brown">Dark Chocolate Survey</h3>'
     
-    h += 'What % of cocao do you like the most: <input id="cocaopct" size=4 value="20"> '
-    
+    h += 'What % of cocao do you like the most: <input id="cocaopct" size=4 value="70"> '
+    h +='<button id="subButton" type="button" class="btn btn-primary">submit</button> '
     if(typeof(sbmApps)=='function'){ // if sbmApps library was loaded
         sbmApps.render(h)
     }else{ // if fib.js is being called without any help
